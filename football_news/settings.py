a7a3c745fa414eca62761008b1326dd2276dcae2
karlaraswanda <karla.raswanda@gmail.com>
2025-11-19 13:50:00 +0700

    add: authentication/ for Tutorial 8

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,11 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "karla-ameera-footballnews.pbp.cs.ui.ac.id"
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "karla-ameera-footballnews.pbp.cs.ui.ac.id",  "10.0.2.2"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://karla-ameera-footballnews.pbp.cs.ui.ac.id"
@@ -51,10 +47,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'corsheaders',
     'main',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -159,3 +158,10 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
